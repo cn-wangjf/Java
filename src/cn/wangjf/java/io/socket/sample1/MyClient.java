b894a5e052cc5763efaf6c93d909710c353be9d0
cn-wangjf <18201804615@163.com>
2015-01-11 19:36:17 +0800

    socket

---
+++ src/cn/wangjf/java/io/socket/sample1/MyClient.java
@@ -0,0 +1,33 @@
+package cn.wangjf.java.io.socket.sample1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * User: wangjf
+ * Date: 15-1-11
+ * Time: 下午6:30
+ */
+public class MyClient {
+
+    public static void main(String[] args) throws Exception {
+        Socket socket = new Socket("localhost", 10000);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream());
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String msg = reader.readLine();
+            System.out.println("client input: " + msg);
+            out.println(msg);
+            out.flush();
+            if (msg.equals("bye")) {
+                break;
+            }
+            System.out.println(in.readLine());  //会阻塞
+        }
+        socket.close();
+    }
+}
---
+++ src/cn/wangjf/java/io/socket/sample1/MyServer.java
@@ -0,0 +1,34 @@
+package cn.wangjf.java.io.socket.sample1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * User: wangjf
+ * Date: 15-1-11
+ * Time: 下午6:29
+ */
+public class MyServer {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(10000);
+        Socket socket = server.accept(); //侦听并接受到此套接字的连接。此方法在连接传入之前一直阻塞。
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream());
+
+        while (true) {
+            String msg = in.readLine();
+            System.out.println(msg);
+            out.println("Server received " + msg);
+            out.flush();
+            if (msg.equals("bye")) {
+                break;
+            }
+        }
+        socket.close();
+    }
+}
